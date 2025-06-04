5769d50858a9419e640b023ba9988af85041d0ce
Blad <BladimirRosane@gmail.com>
2025-06-04 00:15:22 -0300

    mejoras en el código

--- arachnid/tablero.py
+++ arachnid/tablero.py
@@ -1,3 +1,6 @@
+from openpyxl.xml import defusedxml_env_set
+
+
 class Tablero:
     def __init__(self):
         self.columnas = {i : [] for i in range(1,11)} # diccionario con entero como clave y lista como valor. Clave desde 1 a 10
@@ -44,3 +47,73 @@ class Tablero:
             print(f"GANASTE")
             print()
             return
+
+    def prepare_list(self,c1,i):
+        lista1 = self.columnas_de_tablero[c1][-1][i:]
+        self.columnas_de_tablero[c1][-1]= self.columnas_de_tablero[c1][-1][:i]
+        self.columnas_de_tablero[c1][-1].append(lista1)
+
+    def move_to_empty_col(self, col1, col2): # mueve sin reglas, el último elemento de la columma c1 a el último lugar de la columna c2
+        elemento = self.columnas_de_tablero[col1].pop()
+        self.columnas_de_tablero[col2].append(elemento)
+
+    def move_card_to_card(self, col1, col2, mismo_palo=True):# usar solamente cuando el último elemento de las columnas es una carta SOLA
+        if mismo_palo:
+            carta1 = self.columnas_de_tablero[col1].pop()
+            carta2 = self.columnas_de_tablero[col2].pop()
+            self.columnas_de_tablero[col2].append([carta2,carta1])
+        else:
+            carta1 = self.columnas_de_tablero[col1].pop()
+            self.columnas_de_tablero[col2].append(carta1)
+
+    def move_card_to_list(self, col1, col2, mismo_palo=True):
+        if mismo_palo:
+            carta1 = self.columnas_de_tablero[col1].pop()
+            self.columnas_de_tablero[col2][-1].append(carta1)
+        else:
+            carta1 = col1.pop()
+            self.columnas_de_tablero[col2].append(carta1)
+
+    def move_list_to_list(self, col1, col2, mismo_palo=True):
+        if mismo_palo:
+            lista1 = self.columnas_de_tablero[col1].pop()
+            self.columnas_de_tablero[col2][-1].extend(lista1)
+        else:
+            lista1 = col1.pop()
+            self.columnas_de_tablero[col2].append(lista1)
+
+    def move_list_to_card(self, col1, col2, mismo_palo=True):
+        if mismo_palo:
+            lista1 = self.columnas_de_tablero[col1].pop()
+            carta2 = self.columnas_de_tablero[col2].pop()
+            lista1.insert(0,carta2)
+            self.columnas_de_tablero[col2].append(lista1)
+        else:
+            lista1 = self.columnas_de_tablero[col1].pop()
+            self.columnas_de_tablero[col2].append(lista1)
+
+    def fix_col(self,col1, col2):
+        self.lejos(col2)
+        if len(self.columnas_de_tablero[col1]) > 0:
+            if isinstance(self.columnas_de_tablero[col1][-1], list):
+                if len(self.columnas_de_tablero[col1][-1]) == 0:
+                    print("Lista vacía se elimina correctamente, FUNCIONÉ :DDDDDD")
+                    self.columnas_de_tablero[col1] = col1.pop()
+                    print("Tengo que mostrar c1? D:")
+                elif len(self.columnas_de_tablero[col1][-1]) == 1:
+                    print("FUAAA arreglo c1 :D")
+                    print(f"antes: {self.columnas_de_tablero[col1]}")
+                    self.columnas_de_tablero[col1][-1] = self.columnas_de_tablero[col1][-1][0]
+                    print(f"después: {self.columnas_de_tablero[col1]}")
+            else:
+                try:
+                    print("Apuntamos a la carta y la mostramos si no está mostrada:")
+                    print(f"columna antes del intento: {self.columnas_de_tablero[col1]}")
+                    self.columnas_de_tablero[col1][-1].mostrar()
+                    print(f"Columna después: {self.columnas_de_tablero[col1]}")
+                except Exception as e:
+                    print("Manejar exception ::::: ",e)
+        print()
+        print("Me faltó algo? fin del fix :p ¿fixed?")
+
+
